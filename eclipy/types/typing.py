c11c6a4712db256a54ab401a462aabb234aff4ba
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2024-11-11 22:09:33 -0500

    Update typing.py

--- eclipy/types/typing.py
+++ eclipy/types/typing.py
@@ -1,7 +1,31 @@
-class enum:
-    def __init__(self, *args):
-        for i, name in enumerate(args):
-            setattr(self, name, i)
+class Collection:
+    def __init__(self):
+        self._items = []
+        self._is_sorted = False
+
+    def __len__(self):
+        return len(self._items)
+
+    def __iter__(self): 
+        return iter(self._items)
 
     def __repr__(self):
-         return f"<enum: {', '.join([f'{name}={getattr(self, name)}' for name in self.__dict__])}>"
+        return f"Collection({self._items})"
+
+    def __getitem__(self, index):
+        return self._items[index]
+
+    def append(self, item = ..., /):
+        self._items.append(item)
+
+    def clear(self):
+        self._items.clear()
+        self._is_sorted = True
+    
+    def sort(self, reverse=False):
+        self._items.sort(reverse=reverse)
+        self._is_sorted = True
+
+    @property
+    def sorted(self):
+        return self._is_sorted
